f2a3836dc74805bdea13379032d431f7826fd016
Valerio <valerioneri.de@gmail.com>
2017-11-23 12:12:07 +0100

    Create lang_it.js

---
+++ js/lang_it.js
@@ -0,0 +1,9 @@
+var lang_it = {
+	"title": 'Questo è un titolo',
+	"anotherTranslation": "Questa è un'altra traduzione",
+    "changeLanguage": 'Cambia lingua',
+    "buttonText": 'Questo è un pulsante'
+}
+
+// tell the library here comes a new language
+lib_lang.addLang('it');
