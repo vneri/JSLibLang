1ec55defff60d38def8d9e3e8d0cf5b9e7c5d317
Valerio <valerioneri.de@gmail.com>
2017-11-23 12:11:48 +0100

    Create lang_en.js

---
+++ js/lang_en.js
@@ -0,0 +1,9 @@
+var lang_en = {
+	"title": 'This is a title',
+	"anotherTranslation": 'This is another translation',
+    "changeLanguage": 'Change language',
+    "buttonText": 'This is a button'
+}
+
+// tell the library here comes a new language
+lib_lang.addLang('en');
