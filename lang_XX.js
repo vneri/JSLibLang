9984fc49e1c4b0dded70974d97f60c24c428562b
Valerio <valerioneri.de@gmail.com>
2017-11-23 10:54:50 +0100

    Language template file
    Latest example on how to define textes and their translation in one language

---
+++ lang_XX.js
@@ -0,0 +1,10 @@
+// template language file
+// change en to match the language code you want to use
+
+var lang_en = {
+	"title": 'This is a title',
+	"anotherTranslation": 'This is another translation'
+}
+
+// tell the library here comes a new language
+lib_lang.addLang('en');
